1ed6391838e64c4ee0261bdfc24d3fc6817beee2
ChrisMD2 <Charlie.mike.1991@gmail.com>
2021-03-07 16:11:47 -0500

    Added firebase

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 alert("main.js ran");
+
+function testFirebase(str) {
+  var strObj = {
+    string: str
+  };
+  firebase.database().ref('subscription-entries').entries().set(strObj)
+    .then(function(snapshot) {
+      success();
+    }, function(error) {
+      console.log('error' + error);
+      error();
+    });
+}
+
+testFirebase("junk")
